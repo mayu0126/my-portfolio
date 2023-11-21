26523949777538d932cce3c67628d864eafdb837
mayu0126 <ildikopinter0126@gmail.com>
2023-11-21 16:14:39 +0100

    style: modify responsivity of buttons and picture

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -71,7 +71,7 @@ function Landing () {
                     d="M19.54 0c1.356 0 2.46 1.104 2.46 2.472v21.528l-2.58-2.28-1.452-1.344-1.536-1.428.636 2.22h-13.608c-1.356 0-2.46-1.104-2.46-2.472v-16.224c0-1.368 1.104-2.472 2.46-2.472h16.08zm-4.632 15.672c2.652-.084 3.672-1.824 3.672-1.824 0-3.864-1.728-6.996-1.728-6.996-1.728-1.296-3.372-1.26-3.372-1.26l-.168.192c2.04.624 2.988 1.524 2.988 1.524-1.248-.684-2.472-1.02-3.612-1.152-.864-.096-1.692-.072-2.424.024l-.204.024c-.42.036-1.44.192-2.724.756-.444.204-.708.348-.708.348s.996-.948 3.156-1.572l-.12-.144s-1.644-.036-3.372 1.26c0 0-1.728 3.132-1.728 6.996 0 0 1.008 1.74 3.66 1.824 0 0 .444-.54.804-.996-1.524-.456-2.1-1.416-2.1-1.416l.336.204.048.036.047.027.014.006.047.027c.3.168.6.3.876.408.492.192 1.08.384 1.764.516.9.168 1.956.228 3.108.012.564-.096 1.14-.264 1.74-.516.42-.156.888-.384 1.38-.708 0 0-.6.984-2.172 1.428.36.456.792.972.792.972zm-5.58-5.604c-.684 0-1.224.6-1.224 1.332 0 .732.552 1.332 1.224 1.332.684 0 1.224-.6 1.224-1.332.012-.732-.54-1.332-1.224-1.332zm4.38 0c-.684 0-1.224.6-1.224 1.332 0 .732.552 1.332 1.224 1.332.684 0 1.224-.6 1.224-1.332 0-.732-.54-1.332-1.224-1.332z" />
                 </svg>
             </div>
-            <div className='mt-8 flex text-xs xs:text-xs sm:text-xs md:text-sm lg:text-base'>
+            <div className='mt-8 xs:flex-col text-xs xs:text-xs sm:text-xs md:text-xs lg:text-base xl:text-base'>
                 <button className='bg-cyan-700 hover:bg-cyan-600 transition duration-300 ease-in-out text-white font-bold py-2 px-4 rounded-full xs:mr-2 sm:mr-4 md:mr-6 lg:mr-8 xl:mr-10'>
                     Contact Me
                 </button>
@@ -81,7 +81,7 @@ function Landing () {
             </div>
         </div>
 
-        <div className='sm:mr-16 sm:w-1/4 md:mr-24 lg:w-1/3 lg:mr-36 mt-36 text-center flex justify-center items-center'>
+        <div className='w-1/3 sm:w-1/4 lg:w-1/3 mt-36 text-center flex justify-center items-center'>
             <div className='w-36 h-36 xs:w-36 xs:h-36 sm:w-40 sm:h-40 md:w-44 md:h-44 lg:w-80 lg:h-80 xl:w-96 xl:h-96 flex justify-center items-center rounded-full overflow-hidden'>
                 <a href="https://imgur.com/iCTFpiy">
                     <img class="w-full h-full object-cover" src="https://i.imgur.com/iCTFpiy.jpg" alt="me" title="source: imgur.com" />
