66cddd9383b41d1c9f852e9ae0454800a8957f06
mayu0126 <ildikopinter0126@gmail.com>
2023-12-07 23:34:41 +0100

    style: improve responsiveness of Contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -24,9 +24,9 @@ const Contact = ({id}) => {
 
   return(
     <div id={id} className='flex justify-center items-center h-screen bg-slate-900'>
-      <div className='-mt-36 w-1/3 px-2 py-2 mr-20 text-slate-900'>
+      <div className='-mt-36 w-5/6 sm:w-4/6 md:w-1/2 lg:w-1/3 px-2 py-2 text-slate-900'>
         <div className='text-center'>
-          <div className='mb-10 font-bold text-2xl xl:text-4xl text-amber-100'>CONTACT</div>
+          <div className='mb-10 font-bold text-2xl md:text-3xl xl:text-4xl text-amber-100'>CONTACT</div>
 
           <form
             className='text-center mb-16 px-3 py-6 bg-white rounded-2xl'
